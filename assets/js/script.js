dfa166c5de2b706ef1469a222f3e94ee0226415a
Myles Domingo <mylesdomingo@berkeley.edu>
2019-12-04 15:06:02 -0800

    second push

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,20 @@
-new fullpage('#fullpage', {
-	//options here
-	autoScrolling: true,
-	scrollHorizontally: true
+// new fullpage('#fullpage', {
+//     //options here
+//     licenseKey: 'EFD38119-A90641FB-80DC8DA1-1677E764',
+//     sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6']
+// });
+
+// console.log('test');
+
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+        //options here
+        autoScrolling:true,
+        navigation: true,
+        scrollingSpeed: 1000,
+        sectionsColor: ['#12141a', 'orange', '#C0C0C0', '#ADD8E6']
+	});
+
+	//methods
+	$.fn.fullpage.setAllowScrolling(false);
 });
